3fc176aa13d3dd94fc3d7a9bcbe64f2074e6aa71
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2026-01-31 20:32:38 -0500

    (feat) create component Page.
    -> component page contains header, main and footer.
    within main there will be two components called ScoreBoard and Cards.
    
    -> component page has a useEffect() hook to retrieve the all the info needed to create the memory cards, and to set state one the
    component is mounted. It will then feed this info  to the Cards component.
    
    -> Info: because the url of the image doesn't work with the original string, some filtering happens in order to make it work.

---
+++ src/components/gamepage.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Cards } from './cards';
+import ScoreBoard from './scoreboard';
+
+export default function Page() {
+	const [characters, setCharacters] = useState();
+
+	useEffect(() => {
+		fetch('https://api.attackontitanapi.com/titans')
+			.then((response) => response.json())
+			.then((data) => data.results)
+			.then((results) =>
+				setCharacters(
+					results.map((result) => ({
+						name: result.name,
+						id: result.id,
+						img: result.img.split('revision')[0],
+						// split is used to correct the url of the image otherwise it comes out as 404 error.
+					})),
+				),
+			);
+	}, []);
+
+	return (
+		<>
+			<header>
+				<h1>Memory Card Game</h1>
+			</header>
+			<main>
+				<h1>
+					Get points by clicking on an image but don't click on any more than once!
+				</h1>
+				<ScoreBoard />
+				<Cards characters={characters} setCharacters={setCharacters} />
+			</main>
+			<footer>
+				<h1>Tiago Sobral</h1>
+			</footer>
+		</>
+	);
+}
