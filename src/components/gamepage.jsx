14c28655b7b311c6ac0126c76b4e495679d7cf6c
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2026-02-04 13:16:25 -0500

    (feat) add onClick feature w/ handleClick on cards
    -> handleClick checks wether the card has been clicked previously.
    if it has:
      - sets score to 0
      - checks if the bestScore is higher that the current score and if it is it updates the amount
      otherwise it stays that way.
      - sets the cards all values of hit key to be false.
    
    if it hasn't:
      - increases score by 1.
      - sets card hit value to true.
    
    (feat) shuffle function has been created to randomize the characters based on the Fisher-Yates Algorithm.
    
    The reshuffle inside of handleClick will be committed in the next round of changes.

--- src/components/gamepage.jsx
+++ src/components/gamepage.jsx
@@ -11,18 +11,42 @@ export default function Page() {
 		fetch('https://api.attackontitanapi.com/titans')
 			.then((response) => response.json())
 			.then((data) => data.results)
-			.then((results) =>
-				setCharacters(
-					results.map((result) => ({
-						name: result.name,
-						id: result.id,
-						img: result.img.split('revision')[0],
-						// split is used to correct the url of the image otherwise it comes out as 404 error.
-						hit: false,
-					})),
-				),
-			);
+			.then((results) => {
+				let figures = results.map((result) => ({
+					name: result.name,
+					id: result.id,
+					img: result.img.split('revision')[0],
+					// split is used to correct the url of the image otherwise it comes out as 404 error.
+					hit: false,
+				}));
+				// shuffle function is called to use the Fisher-Yates Algorithm, and randomize the characters.
+				return setCharacters(shuffle(figures));
+			});
 	}, []);
+
+	function handleClick(e) {
+		let character = e.target.dataset.name;
+		let hit = e.target.dataset.hit;
+
+		if (hit) {
+			if (bestScore < score) {
+				setBestScore(score);
+			}
+			setScore(0);
+			setCharacters(
+				character.map((element) => {
+					if (element === character) return { ...element, hit: false };
+				}),
+			);
+		} else {
+			setScore(score + 1);
+			setCharacters(
+				character.map((element) => {
+					if (element === character) return { ...element, hit: true };
+				}),
+			);
+		}
+	}
 
 	return (
 		<>
@@ -34,11 +58,29 @@ export default function Page() {
 					Get points by clicking on an image but don't click on any more than once!
 				</h1>
 				<ScoreBoard score={score} bestScore={bestScore} />
-				<Cards characters={characters} setCharacters={setCharacters} />
+				<Cards
+					characters={characters}
+					setCharacters={setCharacters}
+					onClick={handleClick}
+				/>
 			</main>
 			<footer>
 				<h1>Tiago Sobral</h1>
 			</footer>
 		</>
 	);
+}
+
+function shuffle(array) {
+	let shuffledArray = array;
+	for (let i = shuffledArray.length - 1; i > 0; i--) {
+		const random = Math.floor(Math.random() * (i + 1));
+
+		[shuffledArray[i], shuffledArray[random]] = [
+			shuffledArray[random],
+			shuffledArray[i],
+		];
+	}
+
+	return shuffledArray;
 }
